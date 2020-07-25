fe206b9949ef939a91fdcc2a0d2040f552c36b4d
suprithreddy267 <52496136+suprithreddy267@users.noreply.github.com>
2020-07-25 19:22:29 +0530

    Add files via upload

---
+++ suprith-CleanCode/src/main/java/Vishay_CleanCode/suprith.java
@@ -0,0 +1,27 @@
+package suprith_CleanCode.suprith_CleanCode;
+
+
+
+import java.io.*;
+
+import java.io.IOException;
+
+
+public class suprith_CleanCode  
+{
+	
+public static void main(String[] args) throws IOException
+	
+{
+		
+Simple_and_Compound_Interest interest_obj = new Simple_and_Compound_Interest();
+		
+interest_obj.calculateInterest();
+		
+House_Construction_Cost_Estimation house_obj= new House_Construction_Cost_Estimation();
+		
+house_obj.costCalculation();
+	
+}
+
+}
---
+++ suprith-CleanCode/src/test/java/Vishay_CleanCode/Vishay_CleanCode/AppTest.java
@@ -0,0 +1,32 @@
+package suprith_CleanCode.suprith_CleanCode;
+
+
+import static org.junit.Assert.assertTrue;
+
+
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+
+public class AppTest 
+
+{
+    
+/**
+     * Rigorous Test :-)
+     */
+ 
+@Test
+    
+public void shouldAnswerWithTrue()
+    
+{
+        
+assertTrue( true );
+    
+}
+
+}
